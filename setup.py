c24199896ccea05e49d8e4d5410af0891042e76d
davidmezzetti <561939+davidmezzetti@users.noreply.github.com>
2025-08-28 16:20:25 -0400

    Sync with txtai 9.0 API, closes #15

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="txtai.py",
-    version="8.6.0",
+    version="9.0.0",
     author="NeuML",
     description="Python client for txtai",
     long_description=DESCRIPTION,
